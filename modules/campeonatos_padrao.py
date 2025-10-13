6e3089e89cad535120ebac6300ae0b37e4ffb610
Xavi146570 <traderteam2020@gmail.com>
2025-10-13 15:56:03 +0100

    Update campeonatos_padrao.py

--- modules/campeonatos_padrao.py
+++ modules/campeonatos_padrao.py
@@ -1,1 +1,30 @@
+import logging
+from datetime import datetime
+from config import Config
+from telegram_client import TelegramClient
+from utils.api_client import ApiFootballClient
+from data.leagues_config import CAMPEONATOS_LEAGUES
+
+logger = logging.getLogger(__name__)
+
+class CampeonatosPadraoModule:
+    """Módulo para análise de campeonatos padrão"""
+    
+    def __init__(self, telegram_client: TelegramClient, api_client: ApiFootballClient):
+        self.telegram_client = telegram_client
+        self.api_client = api_client
+        self.leagues_config = CAMPEONATOS_LEAGUES
+        logger.info(f"Módulo Campeonatos inicializado com {len(self.leagues_config)} ligas")
+
+    async def execute(self):
+        """Executa a análise de campeonatos"""
+        if not Config.CAMPEONATOS_ENABLED:
+            return
+        
+        logger.info("Executando módulo Campeonatos Padrão...")
+        
+        # A lógica completa será implementada no Passo 3
+        
+        await self.telegram_client.send_admin_message("Módulo Campeonatos executado (esqueleto)")
+        logger.info("Módulo Campeonatos concluído")
 
