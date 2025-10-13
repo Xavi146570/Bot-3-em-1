c15595bad31b0a030c658fc3bb1448e0177dc2d8
Xavi146570 <traderteam2020@gmail.com>
2025-10-13 15:54:03 +0100

    Update elite_teams.py

--- data/elite_teams.py
+++ data/elite_teams.py
@@ -1,1 +1,44 @@
+# Lista completa de times de elite (copiada do seu código original)
+ELITE_TEAMS = {
+    # Premier League (Inglaterra)
+    "Manchester City", "Manchester United", "Liverpool", "Chelsea", "Arsenal",
+    
+    # La Liga (Espanha) 
+    "Atlético de Madrid", "Real Madrid", "Barcelona", "Girona",
+    
+    # Serie A (Itália)
+    "Inter de Milão", "Milan", "Atalanta", "Napoli", "Lazio", "Juventus",
+    
+    # Bundesliga (Alemanha)
+    "Bayern de Munique", "RB Leipzig", "Borussia Dortmund", "Bayer Leverkusen",
+    
+    # Ligue 1 (França)
+    "Lille", "PSG", "Monaco", "Marseille", "Lens", "Brest",
+    
+    # Allsvenskan (Suécia)
+    "Malmö FF", "AIK", "Djurgårdens IF", "Häcken", "Hammarby", "Elfsborg",
+    
+    # Brasileirão
+    "Atlético Mineiro", "Flamengo", "Palmeiras", "Internacional", "Fluminense", 
+    "Grêmio", "Botafogo",
+    
+    # Eredivisie (Holanda)
+    "Ajax", "PSV Eindhoven", "AZ Alkmaar", "Feyenoord", "Twente",
+    
+    # Primeira Liga (Portugal)
+    "Sporting CP", "Porto", "Benfica", "Braga",
+    
+    # Scottish Premiership
+    "Rangers", "Celtic", "Hibernian", "Hearts", "Aberdeen",
+    
+    # Süper Lig (Turquia)
+    "Beşiktaş", "Galatasaray", "Fenerbahçe", "Trabzonspor", "Konyaspor",
+    
+    # MLS (EUA)
+    "New York City FC", "Portland Timbers", "New England Revolution", 
+    "Los Angeles FC", "Philadelphia Union", "Columbus Crew", "FC Cincinnati", "Inter Miami",
+    
+    # Adicione aqui o resto da sua lista completa...
+    # (copie todos os times do seu código original)
+}
 
