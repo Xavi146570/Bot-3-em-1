0a9c28bbcb79af5e64bff96303f3557378c72e34
Xavi146570 <traderteam2020@gmail.com>
2025-10-13 15:55:14 +0100

    Update jogos_elite.py

--- modules/jogos_elite.py
+++ modules/jogos_elite.py
@@ -1,1 +1,31 @@
+import logging
+from datetime import datetime
+from config import Config
+from telegram_client import TelegramClient
+from utils.api_client import ApiFootballClient
+from data.elite_teams import ELITE_TEAMS
+
+logger = logging.getLogger(__name__)
+
+class JogosEliteModule:
+    """Módulo para monitorar jogos de times de elite"""
+    
+    def __init__(self, telegram_client: TelegramClient, api_client: ApiFootballClient):
+        self.telegram_client = telegram_client
+        self.api_client = api_client
+        self.elite_teams = ELITE_TEAMS
+        logger.info(f"Módulo Elite inicializado com {len(self.elite_teams)} times")
+
+    async def execute(self):
+        """Executa o monitoramento de jogos elite"""
+        if not Config.ELITE_ENABLED:
+            return
+        
+        logger.info("Executando módulo Jogos Elite...")
+        
+        # A lógica completa será implementada no Passo 3
+        # Por enquanto, apenas um teste básico
+        
+        await self.telegram_client.send_admin_message("Módulo Elite executado (esqueleto)")
+        logger.info("Módulo Elite concluído")
 
